81b4b8a7335490605cebe2b119437a6fea40da18
jtynes <johannes@inpirical.com>
2017-04-21 12:52:06 -0300

    Making magrittr a dependency

--- tests/test_make_file.R
+++ tests/test_make_file.R
@@ -5,11 +5,7 @@ make_filename <- function(year)
 context("File Loading")
 
 
-# ------------------------------------------------------------------------------
-
-
-"make_filename" %>%
-test_that({
+test_that("make_filename", {
   result <- farsfun::make_filename(2017)
   testthat::expect_is(result, "character")
   testthat::expect_true(length(result) == 1)
