6db1e29c18fa11d63a612271e4e55fbd8776fcd6
jtynes <johannes@inpirical.com>
2017-04-21 12:31:46 -0300

    Reconfiguring testthat to work with Travis CI

--- tests/testthat.R
+++
@@ -1,7 +0,0 @@
-# `testthat` Package Initialization File =======================================
-
-# NOTE: all unit tests can be found under the /testthat subfolder.
-
-library(testthat)
-devtools::load_all("pkg")
-testthat::test_package("pkg")
